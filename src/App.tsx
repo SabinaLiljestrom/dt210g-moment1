ead4ab224fb32a3377ae2d38f391a14ad549386a
Sabina Liljeström <sabina.liljestrom@hotmail.com>
2025-03-17 19:37:22 +0100

    skapat React-app och grundstruktur i projektet genom att ta bort onödiga filer och skpa komponenter

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Välkommen till min React-app! 🚀</h1>
+      <p>Har rensat bort onödiga filer!</p>
+    </div>
+  );
+};
+
+export default App;
